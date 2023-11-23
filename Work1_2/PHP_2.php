b8d4694d9dc35504bbd6743c27ae032ecbb8a3fc
farfels <hento2002@mail.ru>
2023-11-23 20:38:31 +0300

    Edited to true form

--- Work1_2/PHP_2.php
+++ Work1_2/PHP_2.php
@@ -1,12 +1,11 @@
 <?php
 $str = file_get_contents("JSON.json");
 $obj = json_decode($str);
-$toCheckLogin = "Temp1";//$toCheckLogin = readline("Введите логин");
+$toCheckLogin = readline("Введите логин");
 $toCheckLogin = "/".$toCheckLogin."/";
-$toCheckPwd = "Aa123456";//$toCheckPwd = readline("Введите пароль");
+$toCheckPwd = readline("Введите пароль");
 $toCheckPwd = "/".$toCheckPwd."/";
 $Login = false;
-// print $obj->{'users'}[0]->{'login'}; --> Вывод Temp1
 foreach ($obj->{'users'} as $object) {
     if (preg_match($toCheckLogin, $object->{'login'}) == true) {
         if (preg_match($toCheckPwd, $object->{'password'}) == true) {
